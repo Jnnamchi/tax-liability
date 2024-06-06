8ec97b32614ffb8e9dde4b412347da3a072a4a1d
John Nnamchi <jnnamchi@Johns-MacBook-Pro.local>
2024-06-06 11:50:13 -0700

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/dist/',
+  base: '/tax-liability/dist/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
